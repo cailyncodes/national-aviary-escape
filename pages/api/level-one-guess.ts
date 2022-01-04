94c0b3708d66b464457b9798e05698967bc7fcbc
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2022-01-03 22:47:57 -0500

    Install missing types

--- pages/api/level-one-guess.ts
+++ pages/api/level-one-guess.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiResponse } from 'next'
 import Cookies from 'cookies';
 import prepareAnswer from '../../utils/prepare-answer';
 
